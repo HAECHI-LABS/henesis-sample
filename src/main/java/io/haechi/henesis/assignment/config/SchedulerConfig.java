caee140f692073a3e02f10a8d4a97a0476173bd3
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 15:20:46 +0900

    feat: create config and set config file

---
+++ src/main/java/io/haechi/henesis/assignment/config/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package io.haechi.henesis.assignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler poolScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(Runtime.getRuntime().availableProcessors()*2);
+        threadPoolTaskScheduler.setThreadNamePrefix("Scheduler-threadPool");
+        return threadPoolTaskScheduler;
+    }
+}

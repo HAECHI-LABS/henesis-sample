9840b48da37391b1c4974b32a6b672b88c18ccb9
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 20:13:28 +0900

    feat: create application DTO and infra

--- src/main/java/io/haechi/henesis/assignment/application/dto/ValueTransferEventDTO.java
+++ src/main/java/io/haechi/henesis/assignment/application/dto/ValueTransferEventDTO.java
@@ -1,4 +1,15 @@
 package io.haechi.henesis.assignment.application.dto;
 
-public class ValueTransferEventDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class ValueTransferEventDTO {
+
+
 }
---
+++ src/main/java/io/haechi/henesis/assignment/infra/dto/CreateUserWalletRequestDTO.java
@@ -0,0 +1,4 @@
+package io.haechi.henesis.assignment.infra.dto;
+
+public class CreateUserWalletRequestDTO {
+}

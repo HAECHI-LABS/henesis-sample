62f6ba59cf887a5b100218193ae3863523394fb2
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 18:11:58 +0900

    feat: Create Run Application

---
+++ src/main/java/io/haechi/henesis/assignment/AssignmentApplication.java
@@ -0,0 +1,21 @@
+package io.haechi.henesis.assignment;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableScheduling
+@EnableJpaRepositories
+@SpringBootApplication
+@Slf4j
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}

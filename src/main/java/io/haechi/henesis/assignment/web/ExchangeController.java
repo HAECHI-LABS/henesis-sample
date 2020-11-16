73448c87edcc1ffd0521caaa49b4174aa46c0f32
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 18:11:23 +0900

    feat: add Controller

--- src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
+++ src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
@@ -1,16 +1,27 @@
 package io.haechi.henesis.assignment.web;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.haechi.henesis.assignment.application.ExchangeApplicationService;
+import io.haechi.henesis.assignment.application.dto.UserWalletDTO;
+import io.haechi.henesis.assignment.application.dto.WalletCreateRequestDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("exchange")
 public class ExchangeController {
 
+    private final ExchangeApplicationService exchangeApplicationService;
+
+    public ExchangeController(ExchangeApplicationService exchangeApplicationService){
+        this.exchangeApplicationService = exchangeApplicationService;
+    }
 
     //지갑생성
-
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public UserWalletDTO createUserWallet(@RequestBody WalletCreateRequestDTO walletCreateRequestDTO){
+        return exchangeApplicationService.createUserWallet(walletCreateRequestDTO.getMasterWalletId());
+    }
 
     //입금
 
--- src/main/java/io/haechi/henesis/assignment/web/MonitoringController.java
+++ src/main/java/io/haechi/henesis/assignment/web/MonitoringController.java
@@ -1,4 +1,10 @@
 package io.haechi.henesis.assignment.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("monitoring")
 public class MonitoringController {
+
 }

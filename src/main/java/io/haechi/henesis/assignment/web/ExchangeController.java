307bf8fcdc46acdbadb5b7bf7ddd27880bd00ac8
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 20:13:50 +0900

    feat: update exchangeController

--- src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
+++ src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
@@ -1,6 +1,8 @@
 package io.haechi.henesis.assignment.web;
 
 import io.haechi.henesis.assignment.application.ExchangeApplicationService;
+import io.haechi.henesis.assignment.application.dto.FlushedTxDTO;
+import io.haechi.henesis.assignment.application.dto.MasterWalletDTO;
 import io.haechi.henesis.assignment.application.dto.UserWalletDTO;
 import io.haechi.henesis.assignment.application.dto.WalletCreateRequestDTO;
 import org.springframework.http.HttpStatus;
@@ -16,14 +18,34 @@ public class ExchangeController {
         this.exchangeApplicationService = exchangeApplicationService;
     }
 
-    //지갑생성
+    // 사용자 지갑 생성
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
     public UserWalletDTO createUserWallet(@RequestBody WalletCreateRequestDTO walletCreateRequestDTO){
         return exchangeApplicationService.createUserWallet(walletCreateRequestDTO.getMasterWalletId());
     }
 
-    //입금
+    // 마스터 지갑 잔고 조회
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public MasterWalletDTO findMasterWalletBalance(@RequestParam String masterWalletId){
+        return exchangeApplicationService.findMasterWalletBalanceById(masterWalletId);
+    }
+
+    // Flushed 된 트랜잭션 조회
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public FlushedTxDTO findFlushedTx(@RequestParam String txId){
+        return exchangeApplicationService.findFlushedTxByTxId(txId);
+    }
+
+
+    // 코인/토큰 전송하기
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public UserWalletDTO findUserWalletByWalletId(@RequestParam String walletId){
+        return exchangeApplicationService.findUserWalletByWalletId(walletId);
+    }
 
     //출금
 

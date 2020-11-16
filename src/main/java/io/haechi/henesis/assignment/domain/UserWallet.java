32f9e3455b7c883b17a2ea4206fff9e7a01681e6
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-16 20:14:30 +0900

    feat: create UserWallet domain and Wallet Service

--- src/main/java/io/haechi/henesis/assignment/domain/UserWallet.java
+++ src/main/java/io/haechi/henesis/assignment/domain/UserWallet.java
@@ -1,4 +1,12 @@
 package io.haechi.henesis.assignment.domain;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserWallet {
+
 }

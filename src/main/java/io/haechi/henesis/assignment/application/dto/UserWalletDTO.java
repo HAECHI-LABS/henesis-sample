8fa17f3608627c5df6d1807ca30b988218cb312d
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-18 13:01:09 +0900

    fix: update UserWallet domain

--- src/main/java/io/haechi/henesis/assignment/application/dto/UserWalletDTO.java
+++ src/main/java/io/haechi/henesis/assignment/application/dto/UserWalletDTO.java
@@ -1,17 +1,13 @@
 package io.haechi.henesis.assignment.application.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserWalletDTO {
 
     private String walletId;
--- src/main/java/io/haechi/henesis/assignment/domain/UserWallet.java
+++ src/main/java/io/haechi/henesis/assignment/domain/UserWallet.java
@@ -8,5 +8,13 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserWallet {
-
+    private int id;
+    private String walletId;
+    private String walletName;
+    private String walletBalance;
+    private String walletAddress;
+    private String masterWalletId;
+    private String createdAt;
+    private String updatedAt;
+    private String blockchain;
 }

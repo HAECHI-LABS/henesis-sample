6cf2e854f504df3739a9586f53a4b6e88a437829
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-21 23:31:46 +0900

    fix: update exchange function

--- src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
+++ src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("exchange")
+@RequestMapping("/exchange")
 public class ExchangeController {
 
     private final ExchangeApplicationService exchangeApplicationService;
@@ -15,7 +15,6 @@ public class ExchangeController {
         this.exchangeApplicationService = exchangeApplicationService;
     }
 
-
     // 사용자 지갑 생성
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
@@ -23,32 +22,18 @@ public class ExchangeController {
         return exchangeApplicationService.createUserWallet(walletName);
     }
 
-
+    // UserWallet 정보 저장
 
     // 마스터 지갑 잔고 조회
+
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public MasterWalletBalanceDTO findMasterWalletBalance(@RequestParam String masterWalletId){
         return exchangeApplicationService.findMasterWalletBalanceById(masterWalletId);
     }
 
-    // Flushed 된 트랜잭션 조회
-    @GetMapping
-    @ResponseStatus(value = HttpStatus.OK)
-    public FlushedTxDTO findFlushedTx(@RequestParam String txId){
-        return exchangeApplicationService.findFlushedTxByTxId(txId);
-    }
+    // UserWallet  walletBalance 조회
 
 
-    // 사용자 지갑 조회 (walletId)
-    @GetMapping
-    @ResponseStatus(value = HttpStatus.OK)
-    public UserWalletDTO findUserWalletByWalletId(@RequestParam String walletId){
-        return exchangeApplicationService.findUserWalletByWalletId(walletId);
-    }
-
-    //출금
-    @PostMapping
-    @ResponseStatus(value = HttpStatus.OK)
 
 }

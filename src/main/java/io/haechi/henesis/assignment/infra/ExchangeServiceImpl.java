ab430a217516f497bc447aa56ddae07b949ae137
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-23 21:31:32 +0900

    fix: add Transfer Coin function

--- src/main/java/io/haechi/henesis/assignment/infra/ExchangeServiceImpl.java
+++ src/main/java/io/haechi/henesis/assignment/infra/ExchangeServiceImpl.java
@@ -1,25 +1,60 @@
 package io.haechi.henesis.assignment.infra;
 
-import io.haechi.henesis.assignment.domain.ExchangeService;
-import io.haechi.henesis.assignment.domain.FlushedTx;
-import io.haechi.henesis.assignment.domain.MasterWalletBalance;
-import io.haechi.henesis.assignment.domain.UserWallet;
+import io.haechi.henesis.assignment.domain.*;
+import io.haechi.henesis.assignment.domain.repository.UserWalletRepository;
+import io.haechi.henesis.assignment.application.dto.CreateUserWalletDTO;
+import io.haechi.henesis.assignment.application.dto.TransferDTO;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 public class ExchangeServiceImpl implements ExchangeService {
 
-    private final HenesisWalletService henesisWalletService;
+    private final HenesisApiCallService henesisApiCallService;
+    private final UserWalletRepository userWalletRepository;
 
-    public ExchangeServiceImpl(HenesisWalletService henesisWalletService){
-        this.henesisWalletService = henesisWalletService;
+    public ExchangeServiceImpl(HenesisApiCallService henesisApiCallService,
+                               UserWalletRepository userWalletRepository){
+        this.henesisApiCallService = henesisApiCallService;
+        this.userWalletRepository = userWalletRepository;
     }
+
 
 
     @Override
-    public UserWallet createUserWallet(String walletName){
-        return henesisWalletService.createUserWallet(walletName);
+    @Transactional
+    public UserWallet createUserWallet(CreateUserWalletDTO createUserWalletDTO){
+        UserWallet userWallet = henesisApiCallService.createUserWallet(createUserWalletDTO);
+        userWalletRepository.save(userWallet);
+
+        return userWallet;
     }
+
+    @Override
+    @Transactional
+    public Transaction transferCoin(TransferDTO transferDTO) {
+
+        Optional<MasterWalletBalance> masterWalletBalance = henesisApiCallService.getMasterWalletBalance(transferDTO.getTicker());
+        System.out.println("test : "+masterWalletBalance.get());
+        UserWallet userWallet =  userWalletRepository.getUserWalletByWalletId(transferDTO.getUserWalletId());
+
+        if (masterWalletBalance.get().getSpendableAmount().compareTo(transferDTO.getAmount())<0
+        && userWallet.getWalletBalance().compareTo(transferDTO.getAmount())<0){
+            System.out.println("Noooo");
+            throw new IllegalStateException(String.format("잔고 부족 ㅎㅎ.."));
+        }else{
+            userWallet.setWalletBalance(userWallet.getWalletBalance().subtract(transferDTO.getAmount()));
+            userWalletRepository.save(userWallet);
+            Transaction transaction = henesisApiCallService.transfer(transferDTO);
+            return transaction;
+        }
+
+    }
+
 
     @Override
     public UserWallet findUserWalletByWalletId(String walletId) {
@@ -35,4 +70,6 @@ public class ExchangeServiceImpl implements ExchangeService {
     public FlushedTx findFlushedTxByTxId(String txId) {
         return null;
     }
+
+
 }

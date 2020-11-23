ee02e20e7a3894bbee2f1cf5c45e99ebcd933644
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-23 21:30:34 +0900

    fix: update userWallet repo

--- src/main/java/io/haechi/henesis/assignment/domain/repository/UserWalletRepository.java
+++ src/main/java/io/haechi/henesis/assignment/domain/repository/UserWalletRepository.java
@@ -1,4 +1,16 @@
 package io.haechi.henesis.assignment.domain.repository;
 
-public interface UserWalletRepository {
+import io.haechi.henesis.assignment.domain.UserWallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+public interface UserWalletRepository extends JpaRepository<UserWallet, String> {
+
+    @Query("SELECT wallet from UserWallet wallet WHERE wallet.walletId = :walletId")
+    UserWallet getUserWalletByWalletId(@Param("walletId") final String walletId);
+
 }

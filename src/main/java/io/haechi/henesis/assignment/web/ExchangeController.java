38b674b9d7b1ad74773325d530c955a59bcfc75e
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-23 21:31:12 +0900

    fix: update each controller function

--- src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
+++ src/main/java/io/haechi/henesis/assignment/web/ExchangeController.java
@@ -2,38 +2,40 @@ package io.haechi.henesis.assignment.web;
 
 import io.haechi.henesis.assignment.application.ExchangeApplicationService;
 import io.haechi.henesis.assignment.application.dto.*;
+import io.haechi.henesis.assignment.domain.repository.UserWalletRepository;
+import io.haechi.henesis.assignment.application.dto.CreateUserWalletDTO;
+import io.haechi.henesis.assignment.application.dto.TransferDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/exchange")
+@RequestMapping("exchange")
 public class ExchangeController {
 
     private final ExchangeApplicationService exchangeApplicationService;
+    private final UserWalletRepository userWalletRepository;
 
-    public ExchangeController(ExchangeApplicationService exchangeApplicationService){
+    public ExchangeController(ExchangeApplicationService exchangeApplicationService,
+                              UserWalletRepository userWalletRepository){
         this.exchangeApplicationService = exchangeApplicationService;
+        this.userWalletRepository = userWalletRepository;
     }
 
     // 사용자 지갑 생성
     @PostMapping("/create-user-wallet")
     @ResponseStatus(value = HttpStatus.OK)
-    public UserWalletDTO createUserWallet(@RequestBody String walletName){
-        return exchangeApplicationService.createUserWallet(walletName);
+    public CreateWalletDTO createUserWallet(@RequestBody CreateUserWalletDTO createUserWalletDTO){
+        return exchangeApplicationService.createUserWallet(createUserWalletDTO);
     }
 
-    // UserWallet 정보 저장
-
-    // 마스터 지갑 잔고 조회
-
-    @GetMapping
+    // 출금하기
+    @PostMapping("/transfer")
     @ResponseStatus(value = HttpStatus.OK)
-    public MasterWalletBalanceDTO findMasterWalletBalance(@RequestParam String masterWalletId){
-        return exchangeApplicationService.findMasterWalletBalanceById(masterWalletId);
+    public TransferCoinDTO transferCoin(@RequestBody TransferDTO transferDTO){
+        return exchangeApplicationService.transferCoin(transferDTO);
     }
 
-    // UserWallet  walletBalance 조회
 
-
+    // 집금하기
 
 }
--- src/main/java/io/haechi/henesis/assignment/web/MonitoringController.java
+++ src/main/java/io/haechi/henesis/assignment/web/MonitoringController.java
@@ -1,10 +1,5 @@
 package io.haechi.henesis.assignment.web;
 
-import io.haechi.henesis.assignment.application.MonitoringApplicationService;
-import io.haechi.henesis.assignment.application.dto.ResultsDTO;
-import io.haechi.henesis.assignment.application.dto.UserWalletDTO;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController

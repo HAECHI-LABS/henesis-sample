896033d70772a0f4aa91c146f850401642e6b23b
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-26 17:43:50 +0900

    fix: update flushedTx Repository

--- src/main/java/io/haechi/henesis/assignment/domain/repository/FlushedTxRepository.java
+++ src/main/java/io/haechi/henesis/assignment/domain/repository/FlushedTxRepository.java
@@ -2,6 +2,21 @@ package io.haechi.henesis.assignment.domain.repository;
 
 import io.haechi.henesis.assignment.domain.FlushedTx;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+@Repository
 public interface FlushedTxRepository extends JpaRepository<FlushedTx, String> {
+
+    @Transactional
+    @Query("SELECT f FROM FlushedTx f WHERE f.txId = :txId")
+    Optional<FlushedTx> getFlushedTxByTxId(@Param("txId") final String txId);
+
+    @Transactional
+    @Query("SELECT f FROM FlushedTx f WHERE f.txId = :txId")
+    FlushedTx findFlushedTxByTxId(@Param("txId") final String txId);
 }
+

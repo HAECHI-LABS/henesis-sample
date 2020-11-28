a42601c311e3cb0c0ef09eb2bf5486c4be3c383d
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-11-28 22:20:09 +0900

    fix: Service 로직 개편

--- src/main/java/io/haechi/henesis/assignment/AssignmentApplication.java
+++ src/main/java/io/haechi/henesis/assignment/AssignmentApplication.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-@EnableJpaRepositories
 @Slf4j
 public class AssignmentApplication {
 
--- src/main/java/io/haechi/henesis/assignment/application/ExchangeApplicationService.java
+++ src/main/java/io/haechi/henesis/assignment/application/ExchangeApplicationService.java
@@ -2,14 +2,12 @@ package io.haechi.henesis.assignment.application;
 
 import io.haechi.henesis.assignment.application.dto.*;
 import io.haechi.henesis.assignment.domain.*;
-import io.haechi.henesis.assignment.domain.repository.FlushedTxRepository;
-import io.haechi.henesis.assignment.domain.repository.UserWalletRepository;
-
+import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ExchangeApplicationService {
@@ -45,27 +43,40 @@ public class ExchangeApplicationService {
     @Transactional
     public TransferResponseDTO transfer(TransferRequestDTO request) {
 
-        Amount spendableAmount = walletService.getMasterWalletBalance(request.getTicker());
-        UserWallet userWallet = userWalletRepository.getUserWalletByWalletId(request.getUserWalletId());
-
-        if (spendableAmount.compareTo(userWallet.getWalletBalance()) < 0
-                && spendableAmount.compareTo(request.getAmount()) < 0) {
-            throw new IllegalStateException("No Money");
-        } else {
-            walletService.transfer(
-                    request.getAmount(),
-                    request.getTo(),
-                    request.getTicker()
-            );
-
-            userWallet.getWalletBalance().subtract(request.getAmount());
-            userWalletRepository.save(userWallet);
-
+        Optional<UserWallet> userWallet = userWalletRepository.findByWalletId(request.getUserWalletId());
+        if (userWallet.isEmpty()) {
+            throw new IllegalStateException(String.format("Can't find Wallet(ID : %s)",request.getUserWalletId()));
         }
+
+
+        Amount spendableAmount = walletService.getMasterWalletBalance(request.getTicker());
+        Amount walletBalance = userWallet.get().getWalletBalance();
+        Amount amount = request.getAmount();
+
+        System.out.println("amount : "+amount);
+        System.out.println("spendableAmount : "+spendableAmount);
+        System.out.println(userWallet);
+        System.out.println("userWalletBalance : "+walletBalance);
+
+
+        if (spendableAmount.compareTo(walletBalance) < 0
+                && spendableAmount.compareTo(request.getAmount()) < 0) {
+            throw new IllegalStateException("Not Enough Money..!");
+        }
+
+        walletService.transfer(
+                request.getAmount(),
+                request.getTo(),
+                request.getTicker()
+        );
+
+        walletBalance.subtract(amount);
+        userWalletRepository.save(userWallet.get());
+
         return TransferResponseDTO.builder()
-                .walletName(userWallet.getWalletName())
-                .amount(request.getAmount())
-                .walletBalance(userWallet.getWalletBalance())
+                .walletName(userWallet.get().getWalletName())
+                .amount(amount)
+                .walletBalance(walletBalance)
                 .build();
     }
 

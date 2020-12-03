da75726d47ec0b6b9eee9534ac69584296a9e020
Jeemin Kim <42401897+PrrrStar@users.noreply.github.com>
2020-12-03 15:17:31 +0900

    fix: change responseDTO > using static factory method

--- src/main/java/io/haechi/henesis/assignment/application/dto/CreateWalletResponseDTO.java
+++ src/main/java/io/haechi/henesis/assignment/application/dto/CreateWalletResponseDTO.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 
 @Getter
-@Setter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateWalletResponseDTO {
 
@@ -15,8 +12,45 @@ public class CreateWalletResponseDTO {
     private String address;
     private String blockchain;
     private String status;
-    private String error;
-    private String transactionId;
     private String createdAt;
     private String updatedAt;
+
+
+    private CreateWalletResponseDTO (
+            String id,
+            String name,
+            String address,
+            String blockchain,
+            String status,
+            String createdAt,
+            String updatedAt
+    ){
+        this.id =id;
+        this.name =name;
+        this.address = address;
+        this.blockchain = blockchain;
+        this.status =status;
+        this.createdAt =createdAt;
+        this.updatedAt =updatedAt;
+    }
+
+    public static CreateWalletResponseDTO of(
+            String id,
+            String name,
+            String address,
+            String blockchain,
+            String status,
+            String createdAt,
+            String updatedAt
+    ){
+        return new CreateWalletResponseDTO(
+                id,
+                name,
+                address,
+                blockchain,
+                status,
+                createdAt,
+                updatedAt
+        );
+    }
 }
